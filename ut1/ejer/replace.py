9fc0b282ce6a0b9abe75a2cf73e9fe11e2c8abf7
colidom <colidom@outlook.com>
2022-11-12 19:40:52 +0000

    Tipos de datos Añadido ejercicio replace.py

---
+++ ut1/ejer/replace.py
@@ -0,0 +1,10 @@
+song = '''You look so beautiful in this light
+Your silhouette over me
+The way it brings out the blue in your eyes
+Is the Tenerife sea
+And all of the voices surrounding us here
+They just fade out when you take a breath
+Just say the word and I will disappear
+Into the wilderness'''
+
+print(song.replace('voices', 'sounds'))
