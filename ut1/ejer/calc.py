5f2e5d74538ba1d8926e10831c02484df5e116a8
colidom <colidom@outlook.com>
2022-11-12 19:19:48 +0000

    Tipos de datos Añadido ejercicio calc.py

---
+++ ut1/ejer/calc.py
@@ -0,0 +1,18 @@
+num1 = int(input("Please, enter the first number: "))
+num2 = int(input("Please, enter the second number: "))
+
+result = num1 + num2
+print(num1, num2, sep='+', end='=')
+print(result)
+
+result = num1 - num2
+print(num1, num2, sep='+', end='=')
+print(result)
+
+result = num1 * num2
+print(num1, num2, sep='+', end='=')
+print(result)
+
+result = num1 / num2
+print(num1, num2, sep='+', end='=')
+print(result)
