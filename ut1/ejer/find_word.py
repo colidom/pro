5d7411f53a2cc5a1e70aac3c4f12182c54f99d3d
colidom <colidom@outlook.com>
2022-11-15 23:32:21 +0000

    Tipos de datos Añadido ejemplo de métodos oara buscar en string

---
+++ ut1/ejer/find_word.py
@@ -0,0 +1,11 @@
+lyrics = '''Quizás porque mi niñez
+Sigue jugando en tu playa
+Y escondido tras las cañas
+Duerme mi primer amor
+Llevo tu luz y tu olor
+Por dondequiera que vaya'''
+
+print(lyrics.startswith('Quizás'))
+print(lyrics.endswith('Final'))
+print(lyrics.find('amor'))
+print(lyrics.count('mi'))
