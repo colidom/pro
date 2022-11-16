6800d48e4a463be722a1e0a5063b7297828bc4fb
Carlos Oliva <carlos.oliva.dom@outlook.es>
2022-11-16 19:01:16 +0000

    Listas Cambiado nombre de variables

--- ut3/ejer/max_value.py
+++ ut3/ejer/max_value.py
@@ -1,7 +1,7 @@
-list_ = [6, 3, 9, 2, 10, 31, 15, 7]
-index = list_[1]
+values = [6, 3, 9, 2, 10, 31, 15, 7]
+index = values[0]
 
-for i in list_:
+for i in values:
     if i > index:
         index = i
 print(index)
