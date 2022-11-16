bfe5d4096fc15f9dce574809e9b72fa6f1dfafe9
Carlos Oliva <carlos.oliva.dom@outlook.es>
2022-11-16 19:01:46 +0000

    Listas Renombrada variable para dar más coherencia

--- ut3/ejer/max_value.py
+++ ut3/ejer/max_value.py
@@ -1,7 +1,7 @@
 values = [6, 3, 9, 2, 10, 31, 15, 7]
 index = values[0]
 
-for i in values:
-    if i > index:
-        index = i
+for value in values:
+    if value > index:
+        index = value
 print(index)
