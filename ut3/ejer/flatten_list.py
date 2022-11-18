d13560fbbfb4810b2bd8625dade3f793813e901e
Carlos Oliva <carlos.oliva.dom@outlook.es>
2022-11-18 16:29:52 +0000

    Listas Cambiado nombre variable

--- ut3/ejer/flatten_list.py
+++ ut3/ejer/flatten_list.py
@@ -1,8 +1,8 @@
-matrix = [0, 10, [20, 30], 40, 50, [60, 70, 80], [90, 100, 110, 120]]
+elements = [0, 10, [20, 30], 40, 50, [60, 70, 80], [90, 100, 110, 120]]
 
 flat_list = []
 
-for element in matrix:
+for element in elements:
     if type(element) == list:
         flat_list += element
     else:
