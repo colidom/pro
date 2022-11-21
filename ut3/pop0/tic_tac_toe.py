5f61bba012d7d502a8c2e5f6162ae8dfbe57132d
colidom <colidom@outlook.com>
2022-11-21 16:56:08 +0000

    Quitado comentario que sobraba

--- ut3/pop0/tic_tac_toe.py
+++ ut3/pop0/tic_tac_toe.py
@@ -1,13 +1,5 @@
 # Juego Tic-Tac-Toe en Python.
 
-''' Haremos el tablero usando un diccionario 
-    en el que las claves serán la ubicación 
-    (por ejemplo: arriba a la izquierda, a la 
-    mitad a la derecha, etc.) y sus valores 
-    iniciales serán espacios vacíos y después 
-    de cada movimiento  cambiaremos el valor
-    de acuerdo a la elección del jugador. '''
-    
 ''' Crearemos un tablero en blanco y mediante 
     el Numeric keypad (Teclado numérico) iremos
     modificando la ubicación de cada valor mediante
