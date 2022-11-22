1fc90cfb0f2a9f724beb82ebf62eb4011b6f0eb8
Carlos Oliva <carlos.oliva.dom@outlook.es>
2022-11-22 18:11:03 +0000

    Listas Añadida opción 2

--- ut3/ejer/sum_diagonal.py
+++ ut3/ejer/sum_diagonal.py
@@ -1,6 +1,13 @@
 matrix = [[4, 6, 1], [2, 9, 3], [1, 7, 7]]
 
+# Opción 1
 suma = 0
 for i in range(len(matrix)):
     suma += matrix[i][i]
 print(suma)
+
+# Opción 2
+diagonal = []
+for i in range(len(matrix)):
+    diagonal.append(matrix[i][i])
+print(sum(diagonal))
