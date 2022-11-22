d52e12478f4cab25320ac2de323a3ba6bb34b816
Carlos Oliva <carlos.oliva.dom@outlook.es>
2022-11-22 20:10:32 +0000

    Optimizando el programa Usamos reverse() para optimizar la  velocidad

--- ut3/ejer/to_binary.py
+++ ut3/ejer/to_binary.py
@@ -6,6 +6,7 @@ while decimal != 0:
     to_bin.append(decimal % 2)
     decimal //= 2
 
-to_bin = [str(i) for i in to_bin[::-1]]
+to_bin.reverse()
+to_bin = [str(i) for i in to_bin]
 to_bin = "".join(to_bin)
 print(to_bin)
