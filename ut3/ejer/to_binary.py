5133abba54f6d2210a4b3fab54c7a92a77309e71
Carlos Oliva <carlos.oliva.dom@outlook.es>
2022-11-22 19:55:10 +0000

    Optimizando ejercicio

--- ut3/ejer/to_binary.py
+++ ut3/ejer/to_binary.py
@@ -3,8 +3,7 @@ to_bin = []
 
 while decimal != 0:
     module = decimal % 2
-    quotient = decimal // 2
-    to_bin.append(module)
-    decimal = quotient
+    to_bin.append(decimal % 2)
+    decimal //= 2
 
 print(to_bin[::-1])
