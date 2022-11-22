d80f70fd4d7926f1195dcaef70b54a2572a8b21a
Carlos Oliva <carlos.oliva.dom@outlook.es>
2022-11-22 19:10:48 +0000

    FIx bug en solución 2

--- ut3/ejer/powers_of_2.py
+++ ut3/ejer/powers_of_2.py
@@ -7,20 +7,20 @@ print(result)
 
 
 # # No se puede usar funciones ya que no lo hemos dado
-# import sys
+import sys
 
-# BASE = 2
-# n = 10
-# result = []
-
-
-# def powerof2(n):
-#     for i in range(n):
-#         result.append(BASE**i)
-#     print(result)
+BASE = 2
+n = 10
+result = []
 
 
-# powerof2(n)
+def powerof2(n):
+    for i in range(n + 1):
+        result.append(BASE**i)
+    print(result)
+
+
+powerof2(n)
 
 # n = 10
 # exponent_number = [BASE**i for i in range(n + 1)]
