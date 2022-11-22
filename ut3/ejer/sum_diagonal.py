535c7a1d18dd8794b197b3868329023a9a29de91
Carlos Oliva <carlos.oliva.dom@outlook.es>
2022-11-22 18:18:00 +0000

    Añadido ejercicio Opción lista por comprensión

--- ut3/ejer/sum_diagonal.py
+++ ut3/ejer/sum_diagonal.py
@@ -11,3 +11,9 @@ diagonal = []
 for i in range(len(matrix)):
     diagonal.append(matrix[i][i])
 print(sum(diagonal))
+
+# Opción lista por comprensión
+size = len(matrix)
+main_diagonal = [matrix[i][i] for i in range(size)]
+sum_diagonal = sum(main_diagonal)
+print(sum_diagonal)
