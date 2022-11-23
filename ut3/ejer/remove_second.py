b8785c9e6e5cde590b89fe6bd3126364acaf49ed
Carlos Oliva <carlos.oliva.dom@outlook.es>
2022-11-23 19:17:35 +0000

    Añadidos comentarios en ejercicio remove_second.py

--- ut3/ejer/remove_second.py
+++ ut3/ejer/remove_second.py
@@ -4,9 +4,12 @@ Dada una lista, genere otra lista eliminando el segundo elemento de forma repeti
 
 
 def run(items: list) -> list:
+    # Option 1
     # filter = []
     # for i in range(len(items)):
     #     if i % 2 == 0:
     #         filter.append(items[i])
+
+    # Option 2
     filter = items[::2]
     return filter
