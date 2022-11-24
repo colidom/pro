1a67a10cd9dead22dc62fa7ee133ce50197d59e9
colidom <colidom@outlook.com>
2022-11-24 20:43:16 +0000

    Reorganizando tablero

--- ut3/pop0/tic_tac_toe.py
+++ ut3/pop0/tic_tac_toe.py
@@ -22,20 +22,21 @@ while playing == "s":
     print("===============================================")
     player_1 = sys.argv[1:2]
     player_2 = sys.argv[2:]
+
     # Inicializamos variables vacías para pintar tablero
     a = b = c = d = e = f = g = h = i = " "
-    # Pintamos el tablero
 
-    print("\n   --Tablero--   --Guía de juego--   --Jugadores--\n")
-    print("   ", a, "|", b, "|", c, end="       ")
+    # Pintamos el tablero
+    print("\n--Tablero--   --Guía de juego--   --Jugadores--\n")
+    print("", a, "|", b, "|", c, end="       ")
     print(1, "|", 2, "|", 3)
-    print("   ", "---------", end="       ")
+    print("", "---------", end="       ")
     print(f"---------       Player(X): {player_1[0]}   ")
-    print("   ", d, "|", e, "|", f, end="       ")
+    print("", d, "|", e, "|", f, end="       ")
     print(4, "|", 5, "|", 6)
-    print("   ", "---------", end="       ")
+    print("", "---------", end="       ")
     print(f"---------       Player(O): {player_2[0]}   ")
-    print("   ", g, "|", h, "|", i, end="       ")
+    print("", g, "|", h, "|", i, end="       ")
     print(7, "|", 8, "|", 9)
     
     playing = input("\nJuego finalizado, ¿quiere seguir jugando? s/n: ").lower()
