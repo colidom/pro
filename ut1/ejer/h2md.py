b9d8e308489c8546a684ff0d4dd4deb3cf96fd75
colidom <colidom@outlook.com>
2022-11-24 00:55:10 +0000

    Mejora ejercicio

--- ut1/ejer/h2md.py
+++ ut1/ejer/h2md.py
@@ -1,3 +1,13 @@
 html = '<h3>Cadenas de texto</h3>'
-html_start_tag = html.replace('<h3>', '#' * 3)
-print(f"{html_start_tag[:-5]}")
+
+start_index_1 = html.find('<h')
+end_index_2 = html.find('</h')
+
+start_index_2 = start_index_1 + 3
+heading = html[start_index_2 - 1]
+heading_title = html[start_index_2 + 1 : end_index_2]
+
+md_tag = '#' * int(heading)
+
+markdown = f'{md_tag} {heading_title}'
+print(markdown)
