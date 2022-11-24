d5c21b50184f27af486cd13a2102ebabb74f6f0c
colidom <colidom@outlook.com>
2022-11-24 20:17:43 +0000

    Añadido nombre de jugadores

--- ut3/pop0/tic_tac_toe.py
+++ ut3/pop0/tic_tac_toe.py
@@ -7,24 +7,27 @@ Juego Tres en raya
 # Nombre de los jugadores
 playing = "s"
 while playing == "s":
-    print("Bienvenido al juego Piedra - Papel - Tijera\n")
+    print("===============================================")
+    print("| Bienvenido al juego Piedra - Papel - Tijera |")
+    print("===============================================")
     player_1 = input("Jugador (1) por favor introduce tu nombre: ").upper()
-    print(f"Bienvenid@ {player_1} juegas como 'O'")
+    print(f"Jugador (1): {player_1} juegas como 'O'\n")
     player_2 = input("Jugador (2) por favor introduce tu nombre: ").upper()
-    print(f"Bienvenid@ {player_2} juegas como 'X'")
+    print(f"Jugador (2): {player_2} juegas como 'X'\n")
     # Inicializamos variables vacías para pintar tablero
     a = b = c = d = e = f = g = h = i = " "
     # Pintamos el tablero
-    print("\n--Tablero de juego--    --Guía de juego--\n")
-    print("      ", a, "|", b, "|", c, end="            ")
+    print("_______________________________________________")
+    print("\n   --Tablero--   --Guía de juego--   --Jugadores--\n")
+    print("   ", a, "|", b, "|", c, end="       ")
     print(1, "|", 2, "|", 3)
-    print("      ", "---------", end="            ")
-    print("---------")
-    print("      ", d, "|", e, "|", f, end="            ")
+    print("   ", "---------", end="       ")
+    print(f"---------       Player (1): {player_1}   ")
+    print("   ", d, "|", e, "|", f, end="       ")
     print(4, "|", 5, "|", 6)
-    print("      ", "---------", end="            ")
-    print("---------")
-    print("      ", g, "|", h, "|", i, end="            ")
+    print("   ", "---------", end="       ")
+    print(f"---------       Player (2): {player_2}   ")
+    print("   ", g, "|", h, "|", i, end="       ")
     print(7, "|", 8, "|", 9)
     
     playing = input("\nJuego finalizado, ¿quiere seguir jugando? s/n: ").lower()
