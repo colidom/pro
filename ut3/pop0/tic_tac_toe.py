6c4d16a9bd247e83685f18e595f4d489790b0892
colidom <colidom@outlook.com>
2022-11-24 20:35:14 +0000

    Simplificando el código, ahora recibimos los nombres de los jugadores como parçametro

--- ut3/pop0/tic_tac_toe.py
+++ ut3/pop0/tic_tac_toe.py
@@ -4,29 +4,28 @@ Juego Tres en raya
    __|_|__  O X
      | |
 """
+import sys
 # Nombre de los jugadores
 playing = "s"
 while playing == "s":
     print("===============================================")
-    print("| Bienvenido al juego Piedra - Papel - Tijera |")
+    print("  Bienvenido al juego Piedra - Papel - Tijera  ")
     print("===============================================")
-    player_1 = input("Jugador(1) por favor introduce tu nombre: ").upper()
-    print(f"Jugador(1): {player_1} juegas como 'O'\n")
-    player_2 = input("Jugador(2) por favor introduce tu nombre: ").upper()
-    print(f"Jugador(2): {player_2} juegas como 'X'\n")
+    player_1 = sys.argv[1:2]
+    player_2 = sys.argv[2:]
     # Inicializamos variables vacías para pintar tablero
     a = b = c = d = e = f = g = h = i = " "
     # Pintamos el tablero
-    print("_______________________________________________")
+
     print("\n   --Tablero--   --Guía de juego--   --Jugadores--\n")
     print("   ", a, "|", b, "|", c, end="       ")
     print(1, "|", 2, "|", 3)
     print("   ", "---------", end="       ")
-    print(f"---------       Player(1): {player_1}   ")
+    print(f"---------       Player(X): {player_1[0]}   ")
     print("   ", d, "|", e, "|", f, end="       ")
     print(4, "|", 5, "|", 6)
     print("   ", "---------", end="       ")
-    print(f"---------       Player(2): {player_2}   ")
+    print(f"---------       Player(O): {player_2[0]}   ")
     print("   ", g, "|", h, "|", i, end="       ")
     print(7, "|", 8, "|", 9)
     
