ea835c4535b50fd26eac2b77f24c6bf72363fa0f
colidom <colidom@outlook.com>
2022-11-24 16:08:34 +0000

    Actualizado juego Pintamos el tablero y pedimos nombre de los dos jugadores

--- ut3/pop0/tic_tac_toe.py
+++ ut3/pop0/tic_tac_toe.py
@@ -5,20 +5,26 @@ Juego Tres en raya
      | |
 """
 # Nombre de los jugadores
-player_1 = input("Nombre del jugador 1: ")
-player_2 = input("Nombre del jugador 2: ")
-
-# Inicializando variables
-playing = True
-moves = 0
-
-# Inicio del juego
-while playing:
-    print("Elija donde desea colocar su ficha!")
-    row = input("Fila: ")
-    column = input("Columna: ")
-    moves += 1
-    if moves == 9:
-        playing = False
-
-print(f"Fichas en el tablero: {moves}")
+playing = "s"
+while playing == "s":
+    print("Bienvenido al juego Piedra - Papel - Tijera\n")
+    player_1 = input("Jugador (1) por favor introduce tu nombre: ").upper()
+    print(f"Bienvenid@ {player_1} juegas como 'O'")
+    player_2 = input("Jugador (2) por favor introduce tu nombre: ").upper()
+    print(f"Bienvenid@ {player_2} juegas como 'X'")
+    # Inicializamos variables vacías para pintar tablero
+    a = b = c = d = e = f = g = h = i = " "
+    # Pintamos el tablero
+    print("\n--Tablero de juego--    --Guía de juego--\n")
+    print("      ", a, "|", b, "|", c, end="            ")
+    print(1, "|", 2, "|", 3)
+    print("      ", "---------", end="            ")
+    print("---------")
+    print("      ", d, "|", e, "|", f, end="            ")
+    print(4, "|", 5, "|", 6)
+    print("      ", "---------", end="            ")
+    print("---------")
+    print("      ", g, "|", h, "|", i, end="            ")
+    print(7, "|", 8, "|", 9)
+    
+    playing = input("\nJuego finalizado, ¿quiere seguir jugando? s/n: ").lower()
