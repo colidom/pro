a5a0f24a4941447720d2db1b925ab5a57593b432
colidom <colidom@outlook.com>
2022-11-24 22:12:12 +0000

    Añadido bucle para pedir posición por consola

--- ut3/pop0/tic_tac_toe.py
+++ ut3/pop0/tic_tac_toe.py
@@ -33,4 +33,31 @@ while playing == "s":
     print("", g, "|", h, "|", i, end="       ")
     print(7, "|", 8, "|", 9)
 
+    # Lógica del juego
+    loop = 1
+    while loop < 5:
+        user = input("\nIntroduzca un numero de 1-9: ")
+        #Comprobamos si el valor por consola es numérico
+        if user.isdigit():
+            # La lógica de nuestro juego
+           pass
+        else:
+            # Si no es numérico, error y continue al if
+            print("\nError: Solo se admiten valores numéricos")
+            continue
+        # Pintamos el tablero
+        print("\n--Tablero--   --Guía de juego--   --Jugadores--\n")
+        print("", a, "|", b, "|", c, end="       ")
+        print(1, "|", 2, "|", 3)
+        print("", "---------", end="       ")
+        print(f"---------       Player(X): {player_1[0]}   ")
+        print("", d, "|", e, "|", f, end="       ")
+        print(4, "|", 5, "|", 6)
+        print("", "---------", end="       ")
+        print(f"---------       Player(O): {player_2[0]}   ")
+        print("", g, "|", h, "|", i, end="       ")
+        print(7, "|", 8, "|", 9)
+    else:
+        print("Empate")
+
     playing = input("\nJuego finalizado, ¿quiere seguir jugando? s/n: ").lower()
