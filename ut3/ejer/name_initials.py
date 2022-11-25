8cee23b31bf6828476402f5edd19e4446d436608
Carlos Oliva <carlos.oliva.dom@outlook.es>
2022-11-25 16:35:56 +0000

    Finalizado ejercicio name_initials.py

--- ut3/ejer/name_initials.py
+++ ut3/ejer/name_initials.py
@@ -7,13 +7,24 @@ persona pasadas a mayúsculas y con punto al final.
 def run(fullname: str) -> str:
     upper_name = fullname.upper()
     f_name_splitted = upper_name.split(", ")
-    surname_split = f_name_splitted[0].split()
-    f_name_length = len(f_name_splitted)
-    if f_name_length == 3:
+    surname_splited = f_name_splitted[0].split()
+    if len(surname_splited) == 2:
+        f_name = f_name_splitted[1]
+        name_splitted = f_name.split(" ")
+        if len(name_splitted) == 2:
+            name = name_splitted[0]
+            s_name_1 = surname_splited[0]
+            s_name_2 = surname_splited[1]
+            initials = f"{name[0]}.{s_name_1[0]}.{s_name_2[0]}."
+        else:
+            name = f_name_splitted[1]
+            s_name_1 = surname_splited[0]
+            s_name_2 = surname_splited[1]
+            initials = f"{name[0]}.{s_name_1[0]}.{s_name_2[0]}."
+    elif len(surname_splited) == 1:
         name = f_name_splitted[1]
-        s_name_1 = f_name_splitted[0]
-        s_name_2 = f_name_splitted[1]
-        initials = f"{name[0]}.{s_name_1[0]}.{s_name_2[0]}."
+        s_name_1 = surname_splited[0]
+        initials = f"{name[0]}.{s_name_1[0]}."
 
     return initials
 
