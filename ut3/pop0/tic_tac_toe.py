7604037efde54a055f4d71a7e902e8cd82735a76
colidom <colidom@outlook.com>
2022-11-27 22:49:48 +0000

    Implementada doble condicional para comprobar el turno de cada jugador

--- ut3/pop0/tic_tac_toe.py
+++ ut3/pop0/tic_tac_toe.py
@@ -39,60 +39,110 @@ while playing == "s":
 
     # Lógica del juego
     m = n = o = p = q = r = s = t = u = " "
-
+    user1_turn = True
+    user2_turn = False
     loop = 1
     while loop < 9:
-        user_1 = input("\nJugador 1(X) un número de 1-9: ")
-        # Comprobamos si el valor por consola es numérico
-        if user_1.isdigit():
-            # La lógica de nuestro juego
-            x = int(user_1)
-            # Comprobamos si la celda está disponible
-            if (
-                x == m
-                or x == n
-                or x == o
-                or x == p
-                or x == q
-                or x == r
-                or x == s
-                or x == t
-                or x == u
-                or x > 9
-            ):
-                print("\nYa hay una ficha en esta posición, por favor elija otra")
-                continue
+
+        if user1_turn:
+            user_1 = input("\nJugador 1(X) un número de 1-9: ")
+            # Comprobamos si el valor por consola es numérico
+            if user_1.isdigit():
+                # La lógica de nuestro juego
+                x = int(user_1)
+                # Comprobamos si la celda está disponible
+                if (
+                    x == m
+                    or x == n
+                    or x == o
+                    or x == p
+                    or x == q
+                    or x == r
+                    or x == s
+                    or x == t
+                    or x == u
+                    or x > 9
+                ):
+                    print("\nYa hay una ficha en esta posición, por favor elija otra")
+                    continue
+                else:
+                    # Comprobamos el valor intrudición por el jugador 1
+                    if x == 1:
+                        a = "X"
+                        n = 1
+                    if x == 2:
+                        b = "X"
+                        o = 2
+                    if x == 3:
+                        c = "X"
+                        p = 3
+                    if x == 4:
+                        d = "X"
+                        q = 4
+                    if x == 5:
+                        e = "X"
+                        r = 5
+                    if x == 6:
+                        f = "X"
+                        s = 6
+                    if x == 7:
+                        g = "X"
+                        t = 7
+                    if x == 8:
+                        h = "X"
+                        u = 8
+                    if x == 9:
+                        i = "X"
+                        u = 9
             else:
-                # Comprobamos el valor intrudición por el jugador 1
-                if x == 1:
-                    a = "X"
-                    n = 1
-                if x == 2:
-                    b = "X"
-                    o = 2
-                if x == 3:
-                    c = "X"
-                    p = 3
-                if x == 4:
-                    d = "X"
-                    q = 4
-                if x == 5:
-                    e = "X"
-                    r = 5
-                if x == 6:
-                    f = "X"
-                    s = 6
-                if x == 7:
-                    g = "X"
-                    t = 7
-                if x == 8:
-                    h = "X"
-                    u = 8
-                if x == 9:
-                    i = "X"
-                    u = 9
+                # Si no es numérico, error y continue al if
+                print("\nError: Solo se admiten valores numéricos")
+                continue
+            # Pintamos el tablero tras
+            print("\n--Tablero--   --Guía de juego--   --Jugadores--\n")
+            print("", a, "|", b, "|", c, end="       ")
+            print(1, "|", 2, "|", 3)
+            print("", "---------", end="       ")
+            print(f"---------       Player(X): {player_1[0]}   ")
+            print("", d, "|", e, "|", f, end="       ")
+            print(4, "|", 5, "|", 6)
+            print("", "---------", end="       ")
+            print(f"---------       Player(O): {player_2[0]}   ")
+            print("", g, "|", h, "|", i, end="       ")
+            print(7, "|", 8, "|", 9)
+
+            # Comprobamos el resultado del juego
+            if (
+                (a + d + g == "OOO")
+                or (b + e + h == "OOO")
+                or (c + f + i == "OOO")
+                or (a + b + c == "OOO")
+                or (d + e + f == "OOO")
+                or (g + h + i == "OOO")
+                or (a + e + i == "OOO")
+                or (g + e + c == "OOO")
+            ):
+                print(f"Jugador {player_2} ha ganado")
+                break
+            elif (
+                (a + d + g == "XXX")
+                or (b + e + h == "XXX")
+                or (c + f + i == "XXX")
+                or (a + b + c == "XXX")
+                or (d + e + f == "XXX")
+                or (g + h + i == "XXX")
+                or (a + e + i == "XXX")
+                or (g + e + c == "XXX")
+            ):
+                print(f"Jugador {player_1} ha ganado")
+                break
+            user1_turn = False
+            user2_turn = True
+            loop = loop + 1
+        else:
+            user_2 = input("\nJugador 2(O) un número de 1-9: ")
+            if user_2.isdigit():
                 # Comprobamos el valor intrudición por el jugador 2
-                user_2 = input("\nJugador 2(O) un número de 1-9: ")
                 y = int(user_2)
                 # Comprobamos si la celda está disponible
                 if (
@@ -137,50 +187,51 @@ while playing == "s":
                     if y == 9:
                         i = "O"
                         u = 9
+            else:
+                # Si no es numérico, error y continue al if
+                print("\nError: Solo se admiten valores numéricos")
+                continue
+            # Pintamos el tablero tras
+            print("\n--Tablero--   --Guía de juego--   --Jugadores--\n")
+            print("", a, "|", b, "|", c, end="       ")
+            print(1, "|", 2, "|", 3)
+            print("", "---------", end="       ")
+            print(f"---------       Player(X): {player_1[0]}   ")
+            print("", d, "|", e, "|", f, end="       ")
+            print(4, "|", 5, "|", 6)
+            print("", "---------", end="       ")
+            print(f"---------       Player(O): {player_2[0]}   ")
+            print("", g, "|", h, "|", i, end="       ")
+            print(7, "|", 8, "|", 9)
 
-        else:
-            # Si no es numérico, error y continue al if
-            print("\nError: Solo se admiten valores numéricos")
-            continue
-        # Pintamos el tablero tras
-        print("\n--Tablero--   --Guía de juego--   --Jugadores--\n")
-        print("", a, "|", b, "|", c, end="       ")
-        print(1, "|", 2, "|", 3)
-        print("", "---------", end="       ")
-        print(f"---------       Player(X): {player_1[0]}   ")
-        print("", d, "|", e, "|", f, end="       ")
-        print(4, "|", 5, "|", 6)
-        print("", "---------", end="       ")
-        print(f"---------       Player(O): {player_2[0]}   ")
-        print("", g, "|", h, "|", i, end="       ")
-        print(7, "|", 8, "|", 9)
-
-        # Comprobamos el resultado del juego
-        if (
-            (a + d + g == "OOO")
-            or (b + e + h == "OOO")
-            or (c + f + i == "OOO")
-            or (a + b + c == "OOO")
-            or (d + e + f == "OOO")
-            or (g + h + i == "OOO")
-            or (a + e + i == "OOO")
-            or (g + e + c == "OOO")
-        ):
-            print(f"Jugador {player_2} ha ganado")
-            break
-        elif (
-            (a + d + g == "XXX")
-            or (b + e + h == "XXX")
-            or (c + f + i == "XXX")
-            or (a + b + c == "XXX")
-            or (d + e + f == "XXX")
-            or (g + h + i == "XXX")
-            or (a + e + i == "XXX")
-            or (g + e + c == "XXX")
-        ):
-            print(f"Jugador {player_1} ha ganado")
-            break
-        loop = loop + 1
+            # Comprobamos el resultado del juego
+            if (
+                (a + d + g == "OOO")
+                or (b + e + h == "OOO")
+                or (c + f + i == "OOO")
+                or (a + b + c == "OOO")
+                or (d + e + f == "OOO")
+                or (g + h + i == "OOO")
+                or (a + e + i == "OOO")
+                or (g + e + c == "OOO")
+            ):
+                print(f"Jugador {player_2} ha ganado")
+                break
+            elif (
+                (a + d + g == "XXX")
+                or (b + e + h == "XXX")
+                or (c + f + i == "XXX")
+                or (a + b + c == "XXX")
+                or (d + e + f == "XXX")
+                or (g + h + i == "XXX")
+                or (a + e + i == "XXX")
+                or (g + e + c == "XXX")
+            ):
+                print(f"Jugador {player_1} ha ganado")
+                break
+            user2_turn = False
+            user1_turn = True
+            loop = loop + 1
     else:
         print("Empate")
         playing = input("\nJuego finalizado, ¿quiere seguir jugando? s/n: ").lower()
