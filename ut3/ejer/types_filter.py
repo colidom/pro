723857b4f2ddfdf9f30aa7c9770d86e08647d9b4
colidom <colidom@outlook.com>
2022-11-27 20:58:39 +0000

    Añadido tipo booleano

--- ut3/ejer/types_filter.py
+++ ut3/ejer/types_filter.py
@@ -1,16 +1,18 @@
 lista = [1, 2, 3, 4, True, "Hola Mundo"]
 l_int = []
 l_str = []
-unknown_type = []
+b_type = []
 
 for i in lista:
     if type(i) == int:
         l_int.append(i)
     elif type(i) == str:
         l_str.append(i)
+    elif type(i) == bool:
+        b_type.append(i)
     else:
-        unknown_type.append(i)
+        b_type.append(i)
 
 print(l_int)
 print(l_str)
-print(unknown_type)
+print(b_type)
