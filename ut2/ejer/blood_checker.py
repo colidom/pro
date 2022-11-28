e57d9fb4223c982e531348a8f71fe1b6c6a8bcc5
Carlos Oliva <carlos.oliva.dom@outlook.es>
2022-11-28 22:16:34 +0000

    Fixed: SE arregla print

--- ut2/ejer/blood_checker.py
+++ ut2/ejer/blood_checker.py
@@ -8,8 +8,10 @@ if age >= 18 and age <= 65:
         if heartbeat >= 50 and heartbeat <= 110:
             print("Es usted apto para donar sangre")
         else:
-            print("No cumple ninguno de los requisitos para donar sangre")
+            print("SU respiración no es adecuada, no puede donar sangre")
     else:
         print("Su peso es demasiado bajo, no puede donar sangre")
 else:
-    print("NO está en edad de donar sangre, edades comprendidas de 18 a 65 años de edad")
+    print(
+        "NO está en edad de donar sangre, edades comprendidas de 18 a 65 años de edad"
+    )
