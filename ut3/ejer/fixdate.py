2f6e796a6ffbcbaf42366f1b523b1e144b31ca8a
colidom <colidom@outlook.com>
2022-11-29 01:00:40 +0000

    FIXED: Bug en componer la fecha cogemos correctamente el indice de día y mes

--- ut3/ejer/fixdate.py
+++ ut3/ejer/fixdate.py
@@ -1,8 +1,8 @@
 org_date = "12/31/20"
 
 splitted_date = org_date.split("/")
-day = splitted_date[0]
-month = splitted_date[1]
+day = splitted_date[1]
+month = splitted_date[0]
 year = "20" + splitted_date[2]
 
 new_format = "-".join([day, month, year])
