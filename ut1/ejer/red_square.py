f60f672be4186679392ae9a71b521b2897f872ff
Carlos Oliva <carlos.oliva.dom@outlook.es>
2022-12-01 01:05:24 +0000

    WIP: Ejercicio red_square.py

---
+++ ut1/ejer/red_square.py
@@ -0,0 +1,13 @@
+# ****************
+# EL CUADRADO ROJO
+# ****************
+
+
+def run(arc_A: float) -> float:
+    PI = 3.14
+    area = (2 * arc_A / PI) ** 2
+    return area
+
+
+if __name__ == "__main__":
+    run(1)
