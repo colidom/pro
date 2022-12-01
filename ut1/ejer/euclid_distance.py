4fed2ba8d7b5c1bb9b4c47be86f6771a6bde2295
Carlos Oliva <carlos.oliva.dom@outlook.es>
2022-12-01 00:23:34 +0000

    Refactorizado ejercicio a pycheck

--- ut1/ejer/euclid_distance.py
+++ ut1/ejer/euclid_distance.py
@@ -1,7 +1,12 @@
-x1 = 3
-y1 = 5
-x2 = -7
-y2 = -4
+# ******************
+# DISTANCIA EUCLÍDEA
+# ******************
 
-distance = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** (1 / 2)
-print(distance)
+
+def run(x1: float, y1: float, x2: float, y2: float) -> float:
+    distance = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** (1 / 2)
+    return distance
+
+
+if __name__ == "__main__":
+    run(3, 5, -7, -4)
