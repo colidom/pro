c8fe2313006629f8dc4ecaa3f57ae1bf33e1643c
coldiom <colidom@outlook.com>
2022-12-04 00:05:34 +0000

    Añadido ejercicio return_the_day.py

---
+++ ut2/ejer/return_the_day.py
@@ -0,0 +1,18 @@
+day = 7
+
+if day == 1:
+    print("Sunday")
+elif day == 2:
+    print("Monday")
+elif day == 3:
+    print("Tuesday")
+elif day == 4:
+    print("Wednesday")
+elif day == 5:
+    print("Thursday")
+elif day == 6:
+    print("Friday")
+elif day == 7:
+    print("Saturday")
+else:
+    print("Wrong, please enter a number between 1 and 7")
