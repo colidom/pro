a6e6a3318de98e7d79de8d8760bb5f1ac2ea0687
Carlos Oliva <carlos.oliva.dom@outlook.es>
2023-01-11 18:27:49 +0000

    Formateo

--- ut2/ejer/tennis_set.py
+++ ut2/ejer/tennis_set.py
@@ -12,7 +12,7 @@ def run(points: str) -> tuple:
             points_player1 += 1
         else:
             points_player2 += 1
-        
+
         if points_player1 >= 4 and points_player1 - points_player2 >= 2:
             games_player1 += 1
             points_player1 = 0
@@ -23,9 +23,9 @@ def run(points: str) -> tuple:
             points_player2 = 0
 
     if games_player1 == 6 and games_player1 - games_player2 >= 2:
-        print("Player1: ",games_player1)
+        print("Player1: ", games_player1)
     if games_player2 == 6 and games_player2 - games_player1 >= 1:
-        print("Player2: ",games_player2)
+        print("Player2: ", games_player2)
     if games_player1 == 6 and games_player2 == 6:
         print("Tie-Break")
     return games_player1, games_player2
