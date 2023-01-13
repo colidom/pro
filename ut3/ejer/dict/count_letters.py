2bbbc1c4e2b2369955310b0fce47a1e64b81d157
Carlos Oliva <carlos.oliva.dom@outlook.es>
2023-01-13 16:43:44 +0000

    Añadido ejercicio count letters

---
+++ ut3/ejer/dict/count_letters.py
@@ -0,0 +1,12 @@
+# ***************
+# CONTANDO LETRAS
+# ***************
+
+
+def run(sentence: str) -> dict:
+    counter = 'tu código aquí'
+    return counter
+
+
+if __name__ == '__main__':
+    run('boom')
