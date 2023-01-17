7a69ece715479bbbb37717ffa3f5b80d91852a7e
Carlos Oliva <carlos.oliva.dom@outlook.es>
2023-01-17 19:38:36 +0000

    Añadido ejercicio avg_population.py

---
+++ ut3/ejer/dict/avg_population.py
@@ -0,0 +1,16 @@
+# ******************
+# POBLACIÓN PROMEDIO
+# ******************
+
+
+def run(pdata: dict) -> dict:
+
+    for item in pdata.items():
+
+    return avg_data
+
+
+if __name__ == "__main__":
+    run(
+        {"Tokyo": 38140000, "Delhi": 26454000, "Shanghai": 24484000, "Mumbai": 21357000}
+    )
