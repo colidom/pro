66adc102236591156d5cf351c4ce1c3ba04b7f6f
Carlos Oliva <carlos.oliva.dom@outlook.es>
2023-01-18 18:34:49 +0000

    Añadido ejercicio clear_dict_values.py

---
+++ ut3/ejer/dict/clear_dict_values.py
@@ -0,0 +1,14 @@
+# **********************
+# BORRANDO VALORES CLAVE
+# **********************
+
+
+def run(items: dict) -> dict:
+    citems = {}
+    for key in items.keys():
+        citems[key] = []
+    return citems
+
+
+if __name__ == "__main__":
+    run({"C1": [10, 20, 30], "C2": [20, 30, 40], "C3": [12, 34]})
