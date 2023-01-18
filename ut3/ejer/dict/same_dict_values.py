51d9e6164d1323d4b3b8e03d1d806826042d5d99
Carlos Oliva <carlos.oliva.dom@outlook.es>
2023-01-18 18:15:09 +0000

    Añadido ejercicio same_dict_values.py

---
+++ ut3/ejer/dict/same_dict_values.py
@@ -0,0 +1,19 @@
+# ******************************
+# VALORES IGUALES EN DICCIONARIO
+# ******************************
+
+
+def run(items: dict) -> bool:
+    all_same = True
+    index = []
+    for value in items.values():
+        index.append(value)
+        if value == index[0]:
+            all_same = True
+        else:
+            all_same = False
+    return all_same
+
+
+if __name__ == "__main__":
+    run({"a": 1, "b": 1, "c": 1, "d": 1})
