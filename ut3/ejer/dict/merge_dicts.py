904d7c52505047f85b2125944a2ec1bb163da9fd
Carlos Oliva <carlos.oliva.dom@outlook.es>
2023-01-24 19:12:51 +0000

    Añadido ejercicio merge_dict

--- ut3/ejer/dict/merge_dicts.py
+++ ut3/ejer/dict/merge_dicts.py
@@ -5,9 +5,11 @@
 
 def run(d1: dict, d2: dict) -> dict:
     # TU CÓDIGO AQUÍ
-    merged = 'output'
+    merged = d1
+    for key, value in d2.items():
+        merged[key] = value
     return merged
 
 
-if __name__ == '__main__':
-    run({'a': 1, 'b': 2}, {'a': 3, 'c': 4})
+if __name__ == "__main__":
+    run({"a": 1, "b": 2}, {"a": 3, "c": 4})
