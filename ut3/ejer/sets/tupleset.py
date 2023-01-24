316ef0c87f457816e7272511e0f0e96a2c79541f
Carlos Oliva <carlos.oliva.dom@outlook.es>
2023-01-24 22:57:27 +0000

    Añadido ejercicio tupleset.py

---
+++ ut3/ejer/sets/tupleset.py
@@ -0,0 +1,14 @@
+# ******************
+# TUPLAS Y CONJUNTOS
+# ******************
+
+
+def run(input: tuple) -> set:
+    # TU CÓDIGO AQUÍ
+    output = 'output'
+    return output
+
+
+if __name__ == '__main__':
+    run(((4, 3), (8, 2), (7, 5), (8, 2), (9, 1)))
+
