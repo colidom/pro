ebb6c8c0c0f0ff0b20e1e94bdb50fe4151b913ab
Carlos Oliva <carlos.oliva.dom@outlook.es>
2023-01-24 22:52:30 +0000

    Finalizado ejercicio sort_dict.py

--- ut3/ejer/dict/sort_dict.py
+++ ut3/ejer/dict/sort_dict.py
@@ -3,11 +3,16 @@
 # *********************
 
 
-def run(unsorted_items: dict) -> dict:
-    # TU CÓDIGO AQUÍ
-    sorted_items = 'output'
+def run(unsorted_items: dict) -> list:
+    sorted_items = []
+    packed_items = [f"{v}:{k}" for k, v in unsorted_items.items()]
+    packed_items.sort()
+    for item in packed_items:
+        value, key = item.split(":")
+        fixed_item = (key, value)
+        sorted_items.append(fixed_item)
     return sorted_items
 
 
-if __name__ == '__main__':
-    run({'a': 'two', 'b': 'one', 'c': 'three'})
+if __name__ == "__main__":
+    run({"a": "two", "b": "one", "c": "three"})
