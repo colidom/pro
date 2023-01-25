11872b2d2febf4f8174453991866f6c8da262f85
Carlos Oliva <carlos.oliva.dom@outlook.es>
2023-01-25 18:13:44 +0000

    Mejora ejercicio

--- ut3/ejer/sets/tupleset.py
+++ ut3/ejer/sets/tupleset.py
@@ -4,14 +4,20 @@
 
 
 def run(input: tuple) -> set:
-    output = ()
-    first_set = []
-    second_set = []
-    for internal_tuple in input:
-        first_set.append(internal_tuple[0])
-        second_set.append(internal_tuple[1])
+    # Solución 1
+    # output = ()
+    # first_set = []
+    # second_set = []
+    # for internal_tuple in input:
+    #     first_set.append(internal_tuple[0])
+    #     second_set.append(internal_tuple[1])
+    # Solución 2
+    first_set, second_set = set(), set()
+    for first, second in input:
+        first_set.add(first)
+        second_set.add(second)
 
-    output = (set(first_set), set(second_set))
+    output = (first_set, second_set)
     return output
 
 
