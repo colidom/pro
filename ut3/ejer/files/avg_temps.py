d41742e853d8394e990f7b26c529e9289659e037
Carlos Oliva <carlos.oliva.dom@outlook.es>
2023-01-26 18:30:15 +0000

    Añadido ejercicio

---
+++ ut3/ejer/files/avg_temps.py
@@ -0,0 +1,18 @@
+# *******************
+# TEMPERATURAS MEDIAS
+# *******************
+import filecmp
+from pathlib import Path
+
+
+def run(input_path: Path) -> bool:
+    output_path = "data/avg_temps/avg_temps.dat"
+    with open(input_path, "r") as f:
+        for line in f:
+            print(line)
+
+    return filecmp.cmp(output_path, "data/avg_temps/.expected", shallow=False)
+
+
+if __name__ == "__main__":
+    run("data/avg_temps/temperatures.dat")
