ae433cfad4e5846f6d5b7f3c9bad91f9ff3c8a81
Carlos Oliva <carlos.oliva.dom@outlook.es>
2023-01-31 18:54:23 +0000

    Conseguido contador de palabras

--- ut3/ejer/files/wc.py
+++ ut3/ejer/files/wc.py
@@ -7,11 +7,16 @@ from pathlib import Path
 def run(input_path: Path) -> tuple:
     with open(input_path, "r") as file_input:
         num_lines = 0
+        num_words = 0
+
         for line in file_input:
             num_lines += 1
+            lines = line.strip().split()
+            for line in lines:
+                num_words += 1
 
     # len(s.encode('utf8'))
-    num_words = num_bytes = "output"
+    num_bytes = "output"
 
     return num_lines, num_words, num_bytes
 
