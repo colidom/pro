e294c516780190dfdf2f34998116040075d030b6
Carlos Oliva <carlos.oliva.dom@outlook.es>
2023-01-31 19:02:36 +0000

    FInalizado ejercicio

--- ut3/ejer/files/wc.py
+++ ut3/ejer/files/wc.py
@@ -6,17 +6,16 @@ from pathlib import Path
 
 def run(input_path: Path) -> tuple:
     with open(input_path, "r") as file_input:
-        num_lines = 0
-        num_words = 0
+        num_lines = num_words = num_bytes = 0
 
         for line in file_input:
             num_lines += 1
+            byte_encode = len(line.encode("utf8"))
+            num_bytes += byte_encode
+
             lines = line.strip().split()
             for line in lines:
                 num_words += 1
-
-    # len(s.encode('utf8'))
-    num_bytes = "output"
 
     return num_lines, num_words, num_bytes
 
