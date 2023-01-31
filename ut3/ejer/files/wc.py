4076cd82418b88d887fcac184026e21fee1e6e6c
Carlos Oliva <carlos.oliva.dom@outlook.es>
2023-01-31 18:21:04 +0000

    Conseguido contador de líneas

--- ut3/ejer/files/wc.py
+++ ut3/ejer/files/wc.py
@@ -6,13 +6,12 @@ from pathlib import Path
 
 def run(input_path: Path) -> tuple:
     with open(input_path, "r") as f:
-        for line in f:
-            num_lines = []
+        num_lines = 0
+        for _ in f:
+            num_lines += 1
 
-            print(line)
-            break
-
-    num_lines = num_words = num_bytes = "output"
+    # len(s.encode('utf8'))
+    num_words = num_bytes = "output"
 
     return num_lines, num_words, num_bytes
 
