8d2bdebd651163cfc6fd43b379192cb206ad7528
Carlos Oliva <carlos.oliva.dom@outlook.es>
2023-02-01 19:12:54 +0000

    Añadido ejercicio finalizado

--- ut3/ejer/files/txt2md.py
+++ ut3/ejer/files/txt2md.py
@@ -6,11 +6,18 @@ from pathlib import Path
 
 
 def run(text_path: Path) -> bool:
-    md_path = 'data/txt2md/outline.md'
-    # TU CÓDIGO AQUÍ
+    md_path = "data/txt2md/outline.md"
+    with open(text_path, "r") as file_input:
+        with open(md_path, "w") as file_output:
+            for line in file_input:
+                tabs_count = line.count("\t") + 1
+                hashtags = "#" * tabs_count
+                lines = line.strip()
 
-    return filecmp.cmp(md_path, 'data/txt2md/.expected', shallow=False)
+                file_output.write((f"{hashtags} {lines}\n"))
+
+    return filecmp.cmp(md_path, "data/txt2md/.expected", shallow=False)
 
 
-if __name__ == '__main__':
-    run('data/txt2md/outline.txt')
+if __name__ == "__main__":
+    run("data/txt2md/outline.txt")
