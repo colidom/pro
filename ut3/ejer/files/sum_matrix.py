4ede27c04013c37a3eb772018d67c21d8eb08cf2
colidom <colidom@outlook.com>
2023-02-04 14:43:25 +0000

    FUncionando pero no es así como se pide el ejercicio

--- ut3/ejer/files/sum_matrix.py
+++ ut3/ejer/files/sum_matrix.py
@@ -7,21 +7,27 @@ from pathlib import Path
 
 def run(matrix1_path: Path, matrix2_path: Path) -> bool:
     result_path = "data/sum_matrix/result.dat"
+    int_matrix1 = []
+    int_matrix2 = []
     with open(matrix1_path, "r") as file_input1:
-        matrix1 = [number.strip() for number in file_input1]
+        for line in file_input1:
+            for value in line.strip().split():
+                int_value = int(value)
+                int_matrix1.append(int(int_value))
 
     with open(matrix2_path, "r") as file_input2:
-        matrix2 = [number.strip() for number in file_input2]
+        for line in file_input2:
+            for value in line.strip().split():
+                int_value = int(value)
+                int_matrix2.append(int(int_value))
 
     with open(result_path, "w") as file_output:
-        for value1, value2 in zip(matrix1, matrix2):
-            int_val1 = [int(str_val1) for str_val1 in value1.split(" ")]
-            int_val2 = [int(str_val2) for str_val2 in value2.split(" ")]
-            suma = sum(int_val1 + int_val2)
-            file_output.write(f"{suma}\n")
+        for value1, value2 in zip(int_matrix1, int_matrix2):
+            result = value1 + value2
+            file_output.write(f"{result}\n")
 
     return filecmp.cmp(result_path, "data/sum_matrix/.expected", shallow=False)
 
 
 if __name__ == "__main__":
-    run("data/sum_matrix/matrix1.dat", "data/sum_matrix/matrix2.dat")
+    run("./data/sum_matrix/matrix1.dat", "./data/sum_matrix/matrix2.dat")
