d7f01843e79985758a83e50087b75f91e7dffa6c
colidom <colidom@outlook.com>
2023-02-05 20:50:41 +0000

    Aadido ejercicio fallando por un punto

--- ut3/ejer/files/longest_word.py
+++ ut3/ejer/files/longest_word.py
@@ -5,11 +5,17 @@ from pathlib import Path
 
 
 def run(input_path: Path) -> str:
-    # TU CÓDIGO AQUÍ
-    longest_word = 'output'
+    UNNEEDED_CHARS = ",.;:()"
+    longest_word = ""
+    with open(input_path, "r") as file_input:
+        for line in file_input:
+            splitted_text = line.split()
+            for word in splitted_text:
+                if len(word) >= len(longest_word):
+                    longest_word = word
 
     return longest_word
 
 
-if __name__ == '__main__':
-    run('data/longest_word/python.txt')
+if __name__ == "__main__":
+    run("data/longest_word/python.txt")
