3aafd1f82a1039b14e8d1dd6c1d501f079b33b5a
colidom <colidom@outlook.com>
2023-02-05 20:39:29 +0000

    Añadido ejercicio finalizado

--- ut3/ejer/files/sum_matrix.py
+++ ut3/ejer/files/sum_matrix.py
@@ -7,24 +7,34 @@ from pathlib import Path
 
 def run(matrix1_path: Path, matrix2_path: Path) -> bool:
     result_path = "data/sum_matrix/result.dat"
-    int_matrix1 = []
-    int_matrix2 = []
+    matrix1 = []
+    matrix2 = []
+    result = []
+    line_result = []
+
     with open(matrix1_path, "r") as file_input1:
         for line in file_input1:
-            for value in line.strip().split():
-                int_value = int(value)
-                int_matrix1.append(int_value)
+            matrix_line = line.strip().split()
+            matrix1.append(matrix_line)
 
     with open(matrix2_path, "r") as file_input2:
         for line in file_input2:
-            for value in line.strip().split():
-                int_value = int(value)
-                int_matrix2.append(int_value)
+            matrix_line = line.strip().split()
+            matrix2.append(matrix_line)
 
-    with open(result_path, "w") as file_output:
-        for value1, value2 in zip(int_matrix1, int_matrix2):
-            result = value1 + value2
-            file_output.write(f"{result}\n")
+    with open(result_path, "w") as f:
+        index = 0
+        for i, values in enumerate(matrix1):
+            for _ in values:
+                operation = int(matrix1[i][index]) + int(matrix2[i][index])
+                result.append(str(operation))
+                index += 1
+            line_result.append(result)
+            index = 0
+            result = []
+        for values in line_result:
+            line_values = " ".join(values)
+            f.write(f"{line_values}\n")
 
     return filecmp.cmp(result_path, "data/sum_matrix/.expected", shallow=False)
 
