ff66ddae1aab2974d450819fa326867b62f7bc10
colidom <colidom@outlook.com>
2023-02-05 23:49:15 +0000

    Añadida solución ejercicio get_line.py

--- ut3/ejer/files/get_line.py
+++ ut3/ejer/files/get_line.py
@@ -5,11 +5,18 @@ from pathlib import Path
 
 
 def run(input_path: Path, line_no: int) -> str:
-    # TU CÓDIGO AQUÍ
-    line = 'output'
+    lane_list = []
+    with open(input_path, "r") as input_file:
+        for line in input_file:
+            strip_lane = line.strip()
+            lane_list.append([strip_lane])
+        if lane_list[line_no - 1] in lane_list and line_no > 0:
+            line = "".join(lane_list[line_no - 1])
+        else:
+            line = None
 
     return line
 
 
-if __name__ == '__main__':
-    run('data/get_line/nasdaq.txt', 20)
+if __name__ == "__main__":
+    run("data/get_line/nasdaq.txt", 20)
