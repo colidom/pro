8ba00d6e53d09bdbdadbf60e64a3cb2a714cf845
colidom <colidom@outlook.com>
2023-02-05 23:20:23 +0000

    Añadida solución  ejercicio word_preq.py

--- ut3/ejer/files/word_freq.py
+++ ut3/ejer/files/word_freq.py
@@ -5,11 +5,23 @@ from pathlib import Path
 
 
 def run(input_path: Path, lower_bound: int) -> dict:
-    # TU CÓDIGO AQUÍ
-    freq = 'output'
+    with open(input_path, "r") as file_input:
+        freq_1 = {}
+        file = file_input.read()
+        file = file.replace("\n", " ").lower()
+        file_to_list = file.split()
+    for word in file_to_list:
+        if word not in freq_1:
+            freq_1[word] = 1
+        else:
+            freq_1[word] += 1
+    freq = freq_1.copy()
+    for key, value in freq_1.items():
+        if value < lower_bound:
+            del freq[key]
 
     return freq
 
 
-if __name__ == '__main__':
-    run('data/word_freq/cistercian.txt', 9)
+if __name__ == "__main__":
+    run("data/word_freq/cistercian.txt", 9)
