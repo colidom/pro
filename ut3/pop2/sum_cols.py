bebabfe8d60c6b621fe3dbb4e9948fa04a5fd441
colidom <colidom@outlook.com>
2023-02-09 00:38:31 +0000

    Añadido ejercicio sum_cols.py

--- ut3/pop2/sum_cols.py
+++ ut3/pop2/sum_cols.py
@@ -5,11 +5,24 @@ from pathlib import Path
 
 
 def run(data_path: Path) -> tuple:
-    # TU CÓDIGO AQUÍ
-    csum = 'output'
+    data = []
+    with open(data_path, "r") as f:
+        for row in f:
+            fix_row = [int(val) for val in row.strip().split()]
+            data.append(fix_row)
 
+    cols = []
+    for col_index in range(len(data[0])):
+        col = []
+        for row_index in range(len(data)):
+            value = data[row_index][col_index]
+            col.append(value)
+        cols.append(col)
+
+    csum = [sum(col) for col in cols]
+    csum = tuple(csum)
     return csum
 
 
-if __name__ == '__main__':
-    run('data/sum_cols/data1.txt')
+if __name__ == "__main__":
+    run("data/sum_cols/data1.txt")
