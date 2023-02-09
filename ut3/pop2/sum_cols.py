1cff2bec9399b25b5200da45346f496e7874fd5f
colidom <colidom@outlook.com>
2023-02-09 00:16:50 +0000

    Añadido ejercicio sum_cols.py

---
+++ ut3/pop2/sum_cols.py
@@ -0,0 +1,15 @@
+# ****************
+# SUMANDO COLUMNAS
+# ****************
+from pathlib import Path
+
+
+def run(data_path: Path) -> tuple:
+    # TU CÓDIGO AQUÍ
+    csum = 'output'
+
+    return csum
+
+
+if __name__ == '__main__':
+    run('data/sum_cols/data1.txt')
