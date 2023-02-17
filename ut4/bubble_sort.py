d7ccd2b8233459eab44755143a99f21f9cddbfd0
colidom <colidom@outlook.com>
2023-02-17 17:15:48 +0000

    Añadido ejercicio finalizado

--- ut4/bubble_sort.py
+++ ut4/bubble_sort.py
@@ -3,6 +3,18 @@
 # **********************
 
 
-def bubble():
-    # TU CÓDIGO AQUÍ
+def bubble(items):
+    values = items.copy()
+    length_val = len(values)
 
+    for i in range(0, length_val - 1):
+        for j in range(length_val - 1):
+            if values[j] > values[j + 1]:
+                matraca = values[j]
+                values[j] = values[j + 1]
+                values[j + 1] = matraca
+    return values
+
+
+items = [2, 1, 2]
+bubble(items)
