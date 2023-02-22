c57526d8046d13db45d95794308c660e18cbccac
colidom <colidom@outlook.com>
2023-02-22 18:41:49 +0000

    Añadido ejercicio barycenter

---
+++ ut4/func/barycenter.py
@@ -0,0 +1,14 @@
+# **************************
+# BARICENTRO DE UN TRIÁNGULO
+# **************************
+
+
+def run(A: list, B: list, C: list) -> tuple:
+    # TU CÓDIGO AQUÍ
+    x0 = y0 = 'output'
+
+    return x0, y0
+
+
+if __name__ == '__main__':
+    run([4, 6], [12, 4], [10, 10])
