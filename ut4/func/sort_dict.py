ded6cf2be01e8cca43e76821c7ffdf2e5e6457bb
colidom <colidom@outlook.com>
2023-02-23 18:18:09 +0000

    Añadido ejercicio sort_dict.py

---
+++ ut4/func/sort_dict.py
@@ -0,0 +1,14 @@
+# *********************
+# ORDENE MI DICCIONARIO
+# *********************
+
+
+def run(unsorted_items: dict) -> list:
+    # TU CÓDIGO AQUÍ
+    sorted_items = 'output'
+
+    return sorted_items
+
+
+if __name__ == '__main__':
+    run({'a': 'two', 'b': 'one', 'c': 'three'})
