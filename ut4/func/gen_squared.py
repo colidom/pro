0dde54f23966003cbd4764fa656acaba07c62d30
colidom <colidom@outlook.com>
2023-02-28 19:50:10 +0000

    Añadida solución de gen_Squared.py

--- ut4/func/gen_squared.py
+++ ut4/func/gen_squared.py
@@ -1,8 +1,17 @@
 # *******************
 # GENERANDO CUADRADOS
 # *******************
+""" 
+Escriba una función que reciba un parámetro n y que incluya una expresión generadora para calcular los n primeros números enteros elevados al cuadrado.                                          │
+│                                                                                                                                                                                                  │
+│ Notas:                                                                                                                                                                                           │
+│                                                                                                                                                                                                  │
+│  • La función debe devoler una lista de los valores al cuadrado.                                                                                                                                 │
+│  • n ∈ {0, ℤ+}  
+ """
 
 
-def gen_sq():
-    # TU CÓDIGO AQUÍ
-
+def gen_sq(n: int) -> list:
+    evens_gen = (i**2 for i in range(0, n))
+    result = [i for i in evens_gen]
+    return result
