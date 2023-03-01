6d9e52e475cfbee047b3fce1b23ec01f716b718c
colidom <colidom@outlook.com>
2023-03-01 19:11:32 +0000

    Refactorizado

--- ut4/func/money_back.py
+++ ut4/func/money_back.py
@@ -8,13 +8,12 @@ def run(to_give_back: float, available_currencies: list) -> dict:
     money_back = {}
     for money in sorted(available_currencies, reverse=True):
         if to_give_back >= money:
-            money_quantity = to_give_back // money
+            currency_qty = to_give_back // money
             to_give_back = to_give_back % money
-            money_back[money] = money_quantity
+            money_back[money] = currency_qty
     if to_give_back > 0:
         return None
-    else:
-        return money_back
+    return money_back
 
 
 if __name__ == "__main__":
