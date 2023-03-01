26d955de31d9087907193f8bd3c07bfbd37d7ecd
colidom <colidom@outlook.com>
2023-03-01 17:42:36 +0000

    Limpiando ejercicio de docstring

--- ut4/func/money_back.py
+++ ut4/func/money_back.py
@@ -1,13 +1,6 @@
 # ********************
 # AQUÍ TIENE SU VUELTA
 # ********************
-"""
-Un cliente compra un artículo en una tienda con dinero suficiente (mayor o igual) que el importe del artículo. Por lo tanto hay que devolver el cambio. 
-La tienda dispone de una serie concreta de monedas/billetes. El objetivo del ejercicio es devolver el cambio al cliente empezando por la moneda/billete 
-más grande y llegando hasta la más pequeña                           
-    • Si el dinero es justo hay que devolver un diccionario vacío.
-    • Si el cambio no es posible hay que devolver None.
-"""
 
 
 def run(to_give_back: float, available_currencies: list) -> dict:
