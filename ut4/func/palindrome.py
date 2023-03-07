5472a29288b7d96bdfbb045f77d5bcaff2549264
colidom <colidom@outlook.com>
2023-03-07 20:19:13 +0000

    Ejercicio estaba mal, se queda a medias

--- ut4/func/palindrome.py
+++ ut4/func/palindrome.py
@@ -7,7 +7,10 @@ def is_palindrome(text: str) -> bool:
     word = ""
     for letter in text.replace(" ", ""):
         word += letter
-    if word.lower() == word[::-1].lower():
+    if word.lower() == text[::-1].lower():
         return True
     else:
         return False
+
+
+is_palindrome("yo soy")
