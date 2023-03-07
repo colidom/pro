7c50008e997a4bddee718c25657173693d4ba791
colidom <colidom@outlook.com>
2023-03-07 19:56:28 +0000

    Añadida solución ejercicio perfect.py

--- ut4/func/perfect.py
+++ ut4/func/perfect.py
@@ -3,6 +3,11 @@
 # *****************
 
 
-def is_perfect():
-    # TU CÓDIGO AQUÍ
-
+def is_perfect(n: int) -> bool:
+    operation = 0
+    for i in range(1, n):
+        if n % i == 0:
+            operation += i
+        if n == operation:
+            return True
+    return False
