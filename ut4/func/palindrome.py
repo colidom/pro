08d4a6e0a161795f2a5ab132f4b97655f6edcf0b
colidom <colidom@outlook.com>
2023-03-08 01:01:53 +0000

    Ejercicio corregido

--- ut4/func/palindrome.py
+++ ut4/func/palindrome.py
@@ -4,13 +4,9 @@
 
 
 def is_palindrome(text: str) -> bool:
-    word = ""
-    for letter in text.replace(" ", ""):
-        word += letter
-    if word.lower() == text[::-1].lower():
+    phrase = text.lower().replace(" ", "")
+
+    if phrase == phrase[::-1]:
         return True
     else:
         return False
-
-
-is_palindrome("yo soy")
