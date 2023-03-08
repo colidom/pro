b46b3df373f1d38401d9662a627b26b90389c891
colidom <colidom@outlook.com>
2023-03-08 01:26:13 +0000

    Añadida solución

--- ut4/func/pangram.py
+++ ut4/func/pangram.py
@@ -3,6 +3,10 @@
 # ********
 
 
-def is_pangram():
-    # TU CÓDIGO AQUÍ
-
+def is_pangram(text: str) -> bool:
+    ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+    phrase = text.lower().replace(" ", "")
+    for letter in ALPHABET:
+        if letter not in phrase:
+            return False
+    return True
