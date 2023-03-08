a21a04f8b0bc4258c7cd0a799b2bc32a4142f16b
colidom <colidom@outlook.com>
2023-03-08 18:43:02 +0000

    Se quita condición de parada

--- ut4/func/count_vowels.py
+++ ut4/func/count_vowels.py
@@ -6,9 +6,6 @@
 def count_vowels(text: str) -> int:
     VOWELS = "aeiou"
 
-    # Condición de parada
-    if len(text) == 0:
-        return 0
-
     if text[0] in VOWELS:
-        return count_vowels(text[1]) + 1
+        return count_vowels(text[1:]) + 1
+    return text[0]
