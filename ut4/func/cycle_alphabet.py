799713e18cc61406a8dadfc8a922fc4f5e96f52e
colidom <colidom@outlook.com>
2023-03-08 01:27:51 +0000

    Añadido ejercicio cycle_alphabet.py

---
+++ ut4/func/cycle_alphabet.py
@@ -0,0 +1,14 @@
+# *****************
+# ALFABETO CIRCULAR
+# *****************
+
+
+def run(max_letters: int) -> str:
+    # TU CÓDIGO AQUÍ
+    text = 'output'
+
+    return text
+
+
+if __name__ == '__main__':
+    run(0)
