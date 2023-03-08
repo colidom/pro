744c7fdaa377027180b61c98a164d1ec53389840
colidom <colidom@outlook.com>
2023-03-08 18:04:19 +0000

    Añadido ejercicio count_vowels

---
+++ ut4/func/count_vowels.py
@@ -0,0 +1,9 @@
+# *******************************
+# CONTANDO VOCALES (EN RECURSIVO)
+# *******************************
+
+
+def count_vowels(text: str) -> int:
+    VOWELS = "aeiou"
+
+    return count_vowels()
