1cc962711d742c167068c7a4f70d6e58cf2c6bac
colidom <colidom@outlook.com>
2023-03-08 18:02:19 +0000

    Añadidos ejercicios de examen resueltos

---
+++ ut3/pop2/common_words.py
@@ -0,0 +1,28 @@
+# *************************
+# BUSCANDO PALABRAS COMUNES
+# *************************
+import filecmp
+from pathlib import Path
+
+
+def run(input_path: Path) -> bool:
+    n_common = []
+    with open(input_path, 'r') as f:
+        sentences = [line.lower().strip().split() for line in f]
+    for sentence1 in sentences:
+        set1 = set(sentence1)
+        for sentence2 in sentences:
+            set2 = set(sentence2)
+            n_common.append(len(set1 & set2))
+
+    # BLOQUE DE ESCRITURA
+    output_path = 'data/common_words/output.txt'
+    with open(output_path, 'w') as f:
+        for n in n_common:
+            f.write(f'{n}\n')
+
+    return filecmp.cmp(output_path, 'data/common_words/.expected', shallow=False)
+
+
+if __name__ == '__main__':
+    run('data/common_words/minizen.txt')
--- ut3/pop2/sum_rows.py
+++ ut3/pop2/sum_rows.py
@@ -6,13 +6,13 @@ from pathlib import Path
 
 def run(data_path: Path) -> tuple:
     rsum = []
-    with open(data_path, "r") as f:
+    with open(data_path, 'r') as f:
         for row in f:
-            values = [int(val) for val in row.strip().split()]
-            rsum.append(sum(values))
+            numbers = [int(n) for n in row.strip().split()]
+            rsum.append(sum(numbers))
     rsum = tuple(rsum)
     return rsum
 
 
-if __name__ == "__main__":
-    run("data/sum_rows/data1.txt")
+if __name__ == '__main__':
+    run('data/sum_rows/data1.txt')
