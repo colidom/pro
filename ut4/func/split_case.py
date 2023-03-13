ecd02ab87eadb6f0ac2df2201fe2c7abff1b2db7
colidom <colidom@outlook.com>
2023-03-13 01:41:25 +0000

    Solución ejercicio split_case.py

--- ut4/func/split_case.py
+++ ut4/func/split_case.py
@@ -4,4 +4,13 @@
 
 
 def split_case(words: list[str]) -> list[str]:
-    
+    words_lower = []
+    words_upper = []
+
+    for word in words:
+        if word.islower():
+            words_lower.append(word)
+        elif word.isupper():
+            words_upper.append(word)
+
+    return words_lower, words_upper
