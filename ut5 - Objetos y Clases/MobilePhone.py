4f6bcc3fe6a6f00e4766ac6d873e9818f001b966
colidom <colidom@outlook.com>
2023-03-20 00:53:16 +0000

    Añadido ejercicio mobile.py

---
+++ ut5 - Objetos y Clases/MobilePhone.py
@@ -0,0 +1,31 @@
+class MobilePhone:
+    def __init__(self, manufacturer, screen_size, num_cores):
+        self.manufacturer = manufacturer
+        self.screen_size = screen_size
+        self.num_cores = num_cores
+        self.apps = []
+        self.status = 0
+
+    def power_on(self):
+        self.status = 1
+
+    def power_off(self):
+        self.status = 0
+
+    def install_app(self, app):
+        self.apps.append(app)
+
+    def uninstall_app(self, app):
+        self.apps.remove(app)
+
+
+iPhone = MobilePhone("iPhone", 5.8, 2)
+iPhone.power_on()
+iPhone.install_app("Facebook")
+iPhone.install_app("Whatsapp")
+print("Estado del teléfono: ", iPhone.status)
+print("Aplicaciones instaladas: ", iPhone.apps)
+iPhone.uninstall_app("Facebook")
+print("Aplicaciones instaladas: ", iPhone.apps)
+iPhone.power_off()
+print("Estado del teléfono: ", iPhone.status)
