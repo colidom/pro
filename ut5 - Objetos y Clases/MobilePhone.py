9058a4797b22268dd47cabaf108546d630f3ced2
colidom <colidom@outlook.com>
2023-03-22 19:20:11 +0000

    Añadida integración de consumo de batería

--- ut5 - Objetos y Clases/MobilePhone.py
+++ ut5 - Objetos y Clases/MobilePhone.py
@@ -1,3 +1,9 @@
+POWER_CONSUMPTION_ON = 5
+POWER_CONSUMPTION_OFF = 2.5
+POWER_CONSUMPTION_INSTALL = 1
+POWER_CONSUMPTION_UNINSTALL = 1
+
+
 class MobilePhone:
     def __init__(self, manufacturer, screen_size, num_cores):
         self.manufacturer = manufacturer
@@ -5,23 +11,37 @@ class MobilePhone:
         self.num_cores = num_cores
         self.apps = ["Candy Crash", "Messenger"]
         self.status = 0
+        self.battery = 50
 
     def power_on(self):
         self.status = 1
+        self.battery -= POWER_CONSUMPTION_ON
 
     def power_off(self):
         self.status = 0
+        self.battery -= POWER_CONSUMPTION_OFF
 
     def install_app(self, *apps):
         if self.status == 1:
             for app in apps:
                 if app not in self.apps:
                     self.apps.append(app)
+                    self.battery -= POWER_CONSUMPTION_INSTALL
+                else:
+                    self.battery -= POWER_CONSUMPTION_INSTALL
+        else:
+            self.battery -= POWER_CONSUMPTION_INSTALL
 
     def uninstall_app(self, app):
         if self.status == 1:
             if app in self.apps:
                 self.apps.remove(app)
+                self.battery -= POWER_CONSUMPTION_UNINSTALL
+            else:
+                self.battery -= POWER_CONSUMPTION_UNINSTALL
+
+    def recharge_battery(self, power):
+        self.battery += power
 
 
 iphone = MobilePhone("iphone", 5.8, 2)
@@ -36,3 +56,7 @@ print("Aplicaciones instaladas: ", iphone.apps)
 
 iphone.power_off()
 print("Estado del teléfono: ", iphone.status)
+
+print("Batería del teléfono: ", iphone.battery)
+iphone.recharge_battery(5)
+print("Batería del teléfono: ", iphone.battery)
