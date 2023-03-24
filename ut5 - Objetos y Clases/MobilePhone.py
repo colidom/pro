7afb84025b9213f0b7d0842bf476d64334d9f237
colidom <colidom@outlook.com>
2023-03-24 01:41:02 +0000

    Implementados emojis y método de poner música, además de traducciones

--- ut5 - Objetos y Clases/MobilePhone.py
+++ ut5 - Objetos y Clases/MobilePhone.py
@@ -2,6 +2,7 @@ POWER_CONSUMPTION_ON = 5
 POWER_CONSUMPTION_OFF = 2.5
 POWER_CONSUMPTION_INSTALL = 1
 POWER_CONSUMPTION_UNINSTALL = 1
+POWER_CONSUMPTION_MUSIC = 3
 
 
 class MobilePhone:
@@ -11,15 +12,16 @@ class MobilePhone:
         self.num_cores = num_cores
         self.apps = ["Candy Crash", "Messenger"]
         self.status = False
-        self.battery = 50
+        self.battery = 0
 
     def show_info(self):
-        print(f"Fabricante: {self.manufacturer}")
-        print(f"Tamaño de pantalla: {self.screen_size}")
-        print(f"Número de núcleos: {self.num_cores}")
-        print(f"Aplicaciones instaladas: {self.apps}")
-        print(f"Estado de la batería: {self.battery}%")
-        print(f"Encendido: {self.status}")
+        print("🏭", f"Brand: {self.manufacturer}")
+        print("📱", f"Screen size: {self.screen_size}")
+        print("❤️ ", f"Core numbers: {self.num_cores}")
+        print("🧮", f"Installed apps: {self.apps}")
+        print("🔋", f"Battery status: {self.battery}%")
+        print("🔄", f"Power status: {self.status}")
+        print("=======================================")
 
     def switch(self):
         if self.status:
@@ -34,6 +36,7 @@ class MobilePhone:
                 if app not in self.apps:
                     self.apps.append(app)
                     self.battery -= POWER_CONSUMPTION_INSTALL
+                    print(f"✅ Application {app} has been installed successfully")
                 else:
                     self.battery -= POWER_CONSUMPTION_INSTALL
         else:
@@ -43,12 +46,16 @@ class MobilePhone:
         if self.status and self.battery:
             if app in self.apps:
                 self.apps.remove(app)
-                self.apps.append(app + " (actualizada)")
+                self.apps.append(app + " (updated)")
                 self.battery -= POWER_CONSUMPTION_INSTALL
             else:
-                print("La aplicación no está instalada.")
+                print(
+                    "❌ Update error: The application you are trying to update is not installed."
+                )
         else:
-            print("El teléfono no está encendido o no tiene suficiente batería.")
+            print(
+                "❌ Update error: The phone is not switched ON or does not have enough battery."
+            )
 
     def uninstall_app(self, app):
         if self.status == True:
@@ -59,16 +66,28 @@ class MobilePhone:
                 self.battery -= POWER_CONSUMPTION_UNINSTALL
 
     def recharge_battery(self, power):
+        print("🔋", "Chagning battery")
         self.battery += power
 
+    def play_music(self, song_name, duration):
+        if self.status == True:
+            if self.battery > 0:
+                print("⏯️ ", f"Playing song: {song_name} 🎵")
+                self.current_song = song_name
+                self.battery -= POWER_CONSUMPTION_MUSIC * duration
+            else:
+                print("❌ Insufficient battery power to play the song.")
 
-iphone = MobilePhone("iphone", 5.8, 2)
-iphone.switch()
+
+iphone = MobilePhone("iPhone", 5.8, 2)
 iphone.install_app("Facebook", "Instagram")
 iphone.install_app("Whatsapp", "Messenger")
 iphone.uninstall_app("Facebook")
 
 # iphone.switch()
-# iphone.recharge_battery(5)
 iphone.update_app("Whatsapp")
+iphone.show_info()
+iphone.switch()
+iphone.recharge_battery(10)
+iphone.play_music("Symphony of a devil", 6.22)
 iphone.show_info()
