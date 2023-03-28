c6bbf328c7bf86ec0323a7939219cb8c1b3eca0f
colidom <colidom@outlook.com>
2023-03-28 19:57:03 +0100

    Añadido método para bootear el sistema

--- ut4 - Modularidad/Objetos y Clases/os.py
+++ ut4 - Modularidad/Objetos y Clases/os.py
@@ -29,6 +29,10 @@ class OS:
             "/pictures",
         ]
 
+    def boot_system(self):
+        self.booted = True
+        print("Booting the system!")
+
 
 operating_system = OS("pythonOS", "1.0", "5.10.0-19-amd64", 16)
 operating_system.total_installations()
@@ -36,3 +40,4 @@ operating_system.total_installations()
 operating_system.total_installations()
 print(f"Filesystem structure: {operating_system.get_filesystem_tree()}")
 print(f"OS has been installed {operating_system.times_installed} times so far!")
+operating_system.boot_system()
