9dd61ac2e68fbe8ca65df3735ec390a5a37d00b5
colidom <colidom@outlook.com>
2023-03-28 19:33:26 +0100

    Añadida clase os

---
+++ ut4 - Modularidad/Objetos y Clases/os.py
@@ -0,0 +1,28 @@
+class OS:
+
+    times_installed = 0
+
+    def __init__(self, name, version, kernel, ram) -> None:
+        OS.times_installed += 1
+
+        self.name = name
+        self.version = version
+        self.ram = ram
+        self.kernel = kernel
+        self.filesystem = []
+        self.status = False
+        self.booted = False
+
+    @property
+    def required_ram(self) -> float:
+        return 0.2 * self.ram
+
+    @classmethod
+    def total_installations(cls: OS) -> None:
+        print(f"{cls.times_installed} OS installed so far!")
+
+
+operating_system = OS("pythonOS", "1.0", "5.10.0-19-amd64", 16)
+operating_system.filesystem = ["/home", "/downloads", "/documents"]
+operating_system.times_installed
+print(total_installations.times_installed())
