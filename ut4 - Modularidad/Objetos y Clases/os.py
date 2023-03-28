4a3ef6b34252d5aa4ae21f4239f89a3e4997f8ea
colidom <colidom@outlook.com>
2023-03-28 20:15:53 +0100

    implementado método processes

--- ut4 - Modularidad/Objetos y Clases/os.py
+++ ut4 - Modularidad/Objetos y Clases/os.py
@@ -2,6 +2,14 @@ class OS:
 
     times_installed = 0
 
+    @staticmethod
+    def audit(method):
+        def wrapper(self, *args, **kwargs):
+            print(f"Process {self.processes} running by method '{method.__name__}'")
+            return method(self, *args, **kwargs)
+
+        return wrapper
+
     def __init__(self, name, version, kernel, ram) -> None:
 
         self.name = name
@@ -16,6 +24,7 @@ class OS:
         }
         self.status = False
         self.booted = False
+        self.processes = []
 
     @property
     def required_ram(self) -> float:
@@ -32,6 +41,16 @@ class OS:
         self.booted = True
         print("Booting the system!")
 
+    @audit
+    def run_processes(self, *processes):
+        for process in processes:
+            self.processes.append(process)
+
+    @audit
+    def list_process(self):
+        for process in self.processes:
+            print(process)
+
 
 operating_system = OS("pythonOS", "1.0", "5.10.0-19-amd64", 16)
 operating_system.total_installations()
@@ -40,3 +59,4 @@ operating_system.total_installations()
 print(f"Filesystem structure: {operating_system.get_filesystem_tree()}")
 print(f"OS has been installed {operating_system.times_installed} times so far!")
 operating_system.boot_system()
+operating_system.run_processes("apache")
