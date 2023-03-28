1cce4bb025c7d1f855cd9d66cd96938781ef27de
colidom <colidom@outlook.com>
2023-03-28 19:43:14 +0100

    Corregido problema con método de clase

--- ut4 - Modularidad/Objetos y Clases/os.py
+++ ut4 - Modularidad/Objetos y Clases/os.py
@@ -3,7 +3,6 @@ class OS:
     times_installed = 0
 
     def __init__(self, name, version, kernel, ram) -> None:
-        OS.times_installed += 1
 
         self.name = name
         self.version = version
@@ -18,11 +17,13 @@ class OS:
         return 0.2 * self.ram
 
     @classmethod
-    def total_installations(cls: OS) -> None:
-        print(f"{cls.times_installed} OS installed so far!")
+    def total_installations(cls) -> None:
+        OS.times_installed += 1
 
 
 operating_system = OS("pythonOS", "1.0", "5.10.0-19-amd64", 16)
 operating_system.filesystem = ["/home", "/downloads", "/documents"]
-operating_system.times_installed
-print(total_installations.times_installed())
+operating_system.total_installations()
+operating_system.total_installations()
+operating_system.total_installations()
+print(f"OS has been installed {operating_system.times_installed} times so far!")
