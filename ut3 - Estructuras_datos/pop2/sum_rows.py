36ec541015d0653935d69907718aa612847a15b5
colidom <colidom@outlook.com>
2023-04-11 18:03:05 +0100

    Formateo

--- ut3 - Estructuras_datos/pop2/sum_rows.py
+++ ut3 - Estructuras_datos/pop2/sum_rows.py
@@ -6,12 +6,12 @@ from pathlib import Path
 
 def run(data_path: str) -> tuple:
     rsum = []
-    with open(data_path, 'r') as f:
+    with open(data_path, "r") as f:
         for row in f:
             numbers = [int(n) for n in row.strip().split()]
             rsum.append(sum(numbers))
     return tuple(rsum)
 
 
-if __name__ == '__main__':
-    run('data/sum_rows/data1.txt')
+if __name__ == "__main__":
+    run("data/sum_rows/data1.txt")
