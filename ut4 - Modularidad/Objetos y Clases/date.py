9e0701ee2bf651e57656fbc24b3fe51993a79a93
colidom <colidom@outlook.com>
2023-04-13 17:45:28 +0100

    Añadido ejercicio date.py

---
+++ ut4 - Modularidad/Objetos y Clases/date.py
@@ -0,0 +1,40 @@
+class Date:
+    def __init__(self, day: int, month: int, year: int):
+        """Validar día, mes y año. Se comprobará si la fecha es correcta
+        (entre el 1-1-1900 y el 31-12-2050); si el día no es correcto, lo pondrá a 1;
+        si el mes no es correcto, lo pondrá a 1; y si el año no es correcto, lo pondrá a 1900.
+        Ojo con los años bisiestos.
+        """
+        pass
+
+    def is_leap_year(self) -> bool:
+        pass
+
+    def days_in_month(self) -> int:
+        pass
+
+    def delta_days(self) -> int:
+        """Número de días transcurridos desde el 1-1-1900 hasta la fecha"""
+        pass
+
+    def weekday(self) -> int:
+        """día de la semana de la fecha (0 para domingo, ..., 6 para sábado).
+        El 1-1-1900 fue domingo."""
+        pass
+
+    def is_weekend(self) -> bool:
+        pass
+
+    def short_date(self) -> str:
+        """02/09/2003"""
+        pass
+
+    def __str__(self):
+        """martes 2 de septiembre de 2003"""
+        pass
+
+    # operador + suma días a la fecha
+    # operador - resta días a la fecha o calcula la diferencia entre dos fechas
+    # operador == dice si dos fechas son iguales
+    # operador > dice si una fecha es mayor que otra
+    # operador < dice si una fecha es menor que otra
