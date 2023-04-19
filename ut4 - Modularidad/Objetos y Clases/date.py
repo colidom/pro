dec4c8cb8205f37febf9b647b685e5439f036c27
colidom <colidom@outlook.com>
2023-04-19 01:10:15 +0100

    Añadido método weekday

--- ut4 - Modularidad/Objetos y Clases/date.py
+++ ut4 - Modularidad/Objetos y Clases/date.py
@@ -58,11 +58,9 @@ class Date:
 
     @property
     def weekday(self) -> int:
-        ...
-
-    @property
-    def is_weekend(self) -> bool:
-        ...
+        delta_days = self.get_delta_days()
+        weekday = (delta_days + 1) % 7
+        return weekday
 
     @property
     def short_date(self) -> str:
