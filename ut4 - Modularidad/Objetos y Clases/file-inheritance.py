738f7341e82ea6c69f1169bcc337a2caf42af5aa
colidom <colidom@outlook.com>
2023-04-21 16:13:32 +0100

    añadidos docstring para saber que info retorna cada método info()

--- ut4 - Modularidad/Objetos y Clases/file-inheritance.py
+++ ut4 - Modularidad/Objetos y Clases/file-inheritance.py
@@ -11,15 +11,19 @@ class File:
         return sum(len(content for content in self.contents))
 
     def info(self) -> str:
+        """/home/python/vanrossum.mp4 [size=19B]      # self.info() de File"""
         return f"{self.path}[size={self.size}B]"
 
 
 class MediaFile(File):
     def __init__(self, path: str, codec: str, geoloc: tuple, duration: int) -> None:
         super().__init__(path)
-        pass
 
     def info(self) -> str:
+        """Codec: h264                             # ┐
+        Geolocalization: (23.5454, 31.4343)        # ├ self.info() de MediaFile
+        Duration: 487s                             # ┘"""
+
         return ...
 
 
@@ -31,6 +35,7 @@ class VideoFile(MediaFile):
         pass
 
     def info(self) -> str:
+        """Dimensions: (1920, 1080)  # self.info() de VideoFile"""
         return ...
 
 
