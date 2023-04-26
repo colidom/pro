8f623129d7bbb5837bbe570b2e04f19caa754ee7
colidom <colidom@outlook.com>
2023-04-26 18:03:29 +0100

    Añadaida solución

--- ut4 - Modularidad/Objetos y Clases/fibonacci_iterable.py
+++ ut4 - Modularidad/Objetos y Clases/fibonacci_iterable.py
@@ -5,8 +5,10 @@
 
 class FibonacciIterable:
     def __init__(self, n_values_to_generate: int):
-        self.pointer = 0
         self.n_values_to_generate = n_values_to_generate
+        self.pointer = 0
+        self.num1 = 0
+        self.num2 = 1
 
     def __iter__(self):
         return self
@@ -14,6 +16,10 @@ class FibonacciIterable:
     def __next__(self):
         if self.pointer >= self.n_values_to_generate:
             raise StopIteration
+        next_value = self.num1
+        self.num1, self.num2 = self.num2, self.num1 + self.num2
+        self.pointer += 1
+        return next_value
 
 
 def run(n: int) -> list:
