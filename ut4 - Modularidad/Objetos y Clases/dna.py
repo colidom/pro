c15ff34e666e734b9c17171136221c245d6e4c4f
colidom <colidom@outlook.com>
2023-04-26 19:11:00 +0100

    Añadida estructura de ejercicio dna.py

---
+++ ut4 - Modularidad/Objetos y Clases/dna.py
@@ -0,0 +1,36 @@
+class DNA:
+    ADENINE = "A"
+    THYNINE = "T"
+    CYTOSINE = "C"
+    GUANINE = "G"
+
+    def __init__(self, dna_seq) -> None:
+        pass
+
+    def __str__(self) -> str:
+        return f"Objeto"
+
+    @property
+    def calc_adenine(self):
+        pass
+
+    @property
+    def calc_thynine(self):
+        pass
+
+    @property
+    def calc_cytoshine(self):
+        pass
+
+    @property
+    def calc_guanine(self):
+        pass
+
+    def __add__(self):
+        pass
+
+    def __truediv__(self):
+        pass
+
+    def __mul__(self):
+        pass
