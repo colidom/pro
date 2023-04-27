39c963fcb6fbe28dbf9ef609dc0a8b4367f557e1
colidom <colidom@outlook.com>
2023-04-27 17:54:28 +0100

    Añadidos métodos nuevos y propiedad

--- ut4 - Modularidad/Objetos y Clases/dna.py
+++ ut4 - Modularidad/Objetos y Clases/dna.py
@@ -6,6 +6,7 @@ class DNA:
 
     def __init__(self, dna_seq) -> None:
         self.dna_seq = dna_seq
+        self.sequence = 0
 
     @property
     def calc_adenine(self):
@@ -32,5 +33,12 @@ class DNA:
     def get_occurrence_of_each_base(self):
         pass
 
+    @classmethod
+    def build_from_file(self, path):
+        pass
+
+    def dump_to_file(self, path):
+        pass
+
     def __str__(self) -> str:
-        return f"Objeto"
+        return f"{self.sequence}"
