802cb367ed305e2f33267d6f8037f385869f2089
colidom <colidom@outlook.com>
2023-05-23 20:12:39 +0100

    Añadida solución de url_finder

--- ut5 - Procesamiento de texto/regular_expressions.py
+++ ut5 - Procesamiento de texto/regular_expressions.py
@@ -8,7 +8,6 @@ import re
     5. Escriba un programa en Python que obtenga el resultado de una operación entre números enteros positivos. Las operación puede ser suma, resta, multiplicación o división, y puede haber espacios (o no) entre los operandos y el operador.
 
 """
-regex = r"\b[aeiou]\w*\b"
 zen = """
 Bello es mejor que feo.
 Explícito es mejor que implícito.
@@ -30,11 +29,23 @@ Si la implementación es difícil de explicar, es una mala idea.
 Si la implementación es fácil de explicar, puede que sea una buena idea.
 Los espacios de nombres son una gran idea, ¡tengamos más de esos!
 """
+url = """Aprende Python es una web para aprender a programar en Python, puedes acceder mediante los enlaces 
+https://www.aprendepython.es, https://www.aprendepython.es, https://aprendepython.es, http://aprendepython.es 
+pero no desde 
+httpsx://www.aprendepython.es, httpx://www.aprendepython.es, httpsx://aprendepython.es, httpx://aprendepython.es 
+"""
 
 
 def vowel_count(text):
-    return re.findall(regex, text)
+    return re.findall(r"\b[aeiou]\w*\b", text)
+
+
+def url_finder(url):
+    return re.findall(r"https?:\/\/(?:www\.)?[a-zA-Z0-9]+\.[a-z]\S", url)
 
 
 words = vowel_count(zen)
-print(words)
+print(f"Vowels finded: {words} \n")
+
+url = url_finder(url)
+print(f"Url finded: {url} \n")
