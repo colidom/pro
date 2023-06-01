e48d2697f9626742118b6da503b7ffda9c839e61
colidom <colidom@outlook.com>
2023-06-01 19:28:15 +0100

    Añadido regex de clase

--- ut5 - Procesamiento de texto/eval_str_op.py
+++ ut5 - Procesamiento de texto/eval_str_op.py
@@ -1,3 +1,8 @@
 import re
 
-# 5. Escriba un programa en Python que obtenga el resultado de una operación entre números enteros positivos. Las operación puede ser suma, resta, multiplicación o división, y puede haber espacios (o no) entre los operandos y el operador.
+"""5. Escriba un programa en Python que obtenga el resultado de una operación entre números enteros positivos.
+Las operación puede ser suma, resta, multiplicación o división, y puede haber espacios (o no) entre los
+operandos y el operador."""
+
+regex = r"\s*(\d+(?:\.[_\d]+)?\s*([+\-*/])\s*(\d+(?:\.[_\d+])?)\s*$"
+
